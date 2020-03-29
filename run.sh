3358bdd5d1d8c9b4e3891bf3067b2ae87e36f526
LongWei <weilong029@126.com>
2020-03-29 17:09:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
-project_name='resnet50_training_method_plain_market_epoch50_stepsize10_lr_0.1'
-mkdir logs
-mkdir "logs/$project_name"
 CUDA_VISIBLE_DEVICES=1 python main.py \
---project_name $project_name \
---dataset 'market1501-list' \
+--dataset 'market1501' \
 --b 48 \
 --lr 0.1 \
 --a resnet50 \
---training_method plain \
---data_dir '/home/longwei.wl/reid/open_reid_weilong/datasets' \
+--training_method deada \
+--data_dir <path/to/Market-1501> \
 --features 512 \
---ncls 755 \
+--ncls 751 \
 --step_size 10 \
 --epochs 30 
